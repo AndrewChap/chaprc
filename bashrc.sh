dbf5a80040e8e1cce75d45631a9c2819461433e0
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-09-08 09:44:29 -0600

    Add comment and delete whitespace

--- bashrc.sh
+++ bashrc.sh
@@ -15,14 +15,12 @@ function eb(){
     vi ~/.bashrc
 }
 
-# Only show directory in bash prompt, not entire path
+# Only show directory in bash prompt, not entire path, from 
+# https://unix.stackexchange.com/questions/216953/show-only-current-and-parent-directory-in-bash-prompt
 PS1='${PWD#"${PWD%/*/*}/"} \$ '
-
-
-
 
 # set git editor to vim
 export GIT_EDITOR=vim
 
-# Who want's to type out python3?
+# Who wants to type out python3?
 alias py3=python3
