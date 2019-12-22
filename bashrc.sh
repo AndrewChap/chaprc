5078baf4f9bfbdea574320bf31e16fbc05f63cd7
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-12-22 09:29:04 -0700

    Adding GCP-specific functions that help me out

--- bashrc.sh
+++ bashrc.sh
@@ -25,3 +25,33 @@ export GIT_EDITOR=vim
 
 # Who wants to type out python3?
 alias py3=python3
+
+
+# FUNCTIONS SPECIFIC TO MY GOOGLE CLOUD PLATFORM ENVIRONMENT
+export F='/home/amchap06/python-docs-samples/appengine/flexible'
+# cd to flex app source and start up the virtual environment
+function w(){
+    echo gcloud config set project andrewchap
+    echo cd ~/website
+    echo source helpers.sh
+    gcloud config set project andrewchap
+    cd ~/website
+    source helpers.sh
+}
+function m(){
+    echo cd $F/mortapp
+    echo source ~/envm/bin/activate
+    echo gcloud config set project mortfl
+    echo source helpers.sh
+    cd $F/mortapp
+    source ~/envm/bin/activate
+    gcloud config set project mortfl
+    source helpers.sh
+}
+function s(){
+    cd ~/stock-comparer
+    source env-s/bin/activate
+    gcloud config set project stockcompare
+    source helpers.sh
+}
+
